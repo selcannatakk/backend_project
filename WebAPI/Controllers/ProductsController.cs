a88dc4528045000d7cd3d44cc31a1015d382543b
SELCAN ATAK <192803025@ogr.cbu.edu.tr>
2021-04-13 13:48:22 +0300

    -

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]    
+    [ApiController]               //Attrıbute 
+    public class ProductsController : ControllerBase
+    {
+        // Loosely Coupled - gevşek bagımlılık- soyuta bagımlılık
+        // Naming convertion c# and java da var 
+        //Dependecy chain - bagımlılık  
+        // IProductService productService = new ProductManager(new EfProductDal());
+        //IoC Container -- inversion of control -- degişim  // arka planda senın yerıne new() ler
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public List<Product> Get()
+        { 
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
